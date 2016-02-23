4958f92e20d151d1e0de34816943f2833fefd85e
cmagnus <cmagnus@gmail.com>
2016-02-23 14:42:31 -0500

    End of class day 1: we've set up our data and initialized it. We still have some issues with our print. Those need fixing. Then we can build our recursive function.

--- Hanoi.java
+++ Hanoi.java
@@ -1,9 +1,70 @@
  
 public class Hanoi 
-{
+{  // number of discs
+   public static final int MAX_DISCS = 99;
+   public static final int NUM_TOWERS = 99;
+   // lets be reckless and skip getters/setters and make it public
+   // bad form, but we'll do it just this once
+   public int[][] towers = new int[NUM_TOWERS][MAX_DISCS];
+   public int numDiscs;
+   
+   
+   // constructor that initializes array
+   // we'll be rude again and not do default constructor
+   public Hanoi(int size) {
+     this.numDiscs = size;
+     
+     // make 2nd & 3rd towers all 0s
+     for (int i = 1; i < NUM_TOWERS; i++) { // 3 columns
+       for (int j = 0; j < size; j++) { // size rows
+         this.towers[i][j] = 0; 
+       }
+     }
+     
+     // initialize first tower
+     for (int i = 0; i < size; i++) {
+       this.towers[i][0] = size - i;
+     }
+   } //constructor: Hanoi
+
+   // toString to help with print
+   public String toString() {
+     String out = "";
+     for (int i = 0; i < NUM_TOWERS; i++) {
+       for (int j = 0; j < this.numDiscs; j++) {
+         out = out + this.towers[i][j] + " ";
+       }
+       out = out + "\n";
+     }
+     return(out);
+   } // method toString
+   
+   // print the current state
+   public void print() {
+     System.out.println(this);
+   }
+   
+  
+  
+  
+  // sorts discs to new area
+  public void moveTower() {
+    // base case
+    
+    
+    // recursive case
+    
+  }// method: moveTower
+  
+  // TODO:
+  // printing upside down; fix that
+  // print too many 0s
+  // actually need to make recursive method.
   
   // the main method
   public static void main(String[] args) {
+    Hanoi tower = new Hanoi(3);
+    tower.print();
    
   } // method: main
     
